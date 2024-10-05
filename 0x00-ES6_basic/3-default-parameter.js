0bc9b0d37c52b8e47b53681a8604a27093f93bce
yassineboucham <yassineboucham2002@gmail.com>
2024-10-05 09:31:06 +0100

    task 3

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+    if (expansion1989 === undefined) {
+      expansion1989 = 89;
+    }
+    if (expansion2019 === undefined) {
+      expansion2019 = 19;
+    }
+    return initialNumber + expansion1989 + expansion2019;
+  }
