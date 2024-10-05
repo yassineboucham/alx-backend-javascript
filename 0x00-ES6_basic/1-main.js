90abef28929da64432051eff61446a546ae97a4e
yassineboucham <yassineboucham2002@gmail.com>
2024-10-05 08:52:17 +0100

    all files

---
+++ 0x00-ES6_basic/1-main.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/extensions
+import taskBlock from './1-block-scoped.js';
+
+test('returns the right values', () => {
+    expect(taskBlock(true)).toEqual([false, true]);
+    expect(taskBlock(false)).toEqual([false, true]);
+});
