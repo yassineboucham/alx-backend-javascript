957ffc1ca226d56bace8f783c1eb31955d44b619
yassineboucham <yassineboucham2002@gmail.com>
2024-10-06 18:44:24 +0100

    9

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,1 +1,16 @@
-#!/usr/bin/node
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
