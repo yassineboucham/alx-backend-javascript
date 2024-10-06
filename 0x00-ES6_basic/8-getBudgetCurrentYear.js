8e7849a241da592bf444f8bcec97e54d161eb908
yassineboucham <yassineboucham2002@gmail.com>
2024-10-06 17:40:49 +0100

    8

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+const getCurrentYear = () => {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {};
+
+  budget[`income-${getCurrentYear()}`] = income;
+  budget[`gdp-${getCurrentYear()}`] = gdp;
+  budget[`capita-${getCurrentYear()}`] = capita;
+
+  return budget;
+}
