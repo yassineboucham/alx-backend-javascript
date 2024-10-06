69074d45c1c3d68bd1716f4046888a4c54252d27
yassineboucham <yassineboucham2002@gmail.com>
2024-10-06 19:57:42 +0100

    10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+export default function appendToEachArrayValue(array, appendString) {
+  let i = 0;
+  for (const value of array) {
+    // loop in
+    array[i] = appendString + value;
+    i += 1;
+  }
+
+  return array;
+}
