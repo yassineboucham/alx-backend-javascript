bc2bbe5032b540f425a53d402e2bd4f32ab92f6f
yassineboucham <yassineboucham2002@gmail.com>
2024-10-10 13:02:37 +0100

    solve

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,4 +1,4 @@
 export default function divideFunction(numerator, denominator) {
-    if (denominator) return (numerator / denominator);
-    throw Error('cannot divide by 0');
+  if (denominator) return (numerator / denominator);
+  throw Error('cannot divide by 0');
 }
