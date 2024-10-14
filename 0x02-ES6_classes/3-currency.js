87bf4a6e81efdc36f94244366f081fa198bda053
yassineboucham <yassineboucham2002@gmail.com>
2024-10-14 12:33:07 +0100

    3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,1 +1,26 @@
-#!/usr/bin/node
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
