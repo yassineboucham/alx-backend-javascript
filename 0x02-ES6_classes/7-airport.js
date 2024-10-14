64af48f8ad51208c6a1e3f724da628dcc00c5843
yassineboucham <yassineboucham2002@gmail.com>
2024-10-14 12:55:41 +0100

    7

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,1 +1,26 @@
-#!/usr/bin/node
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
