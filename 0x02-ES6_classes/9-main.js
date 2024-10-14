65fb2fff2ff21ebf3495cd2cb917ad980ecdb285
yassineboucham <yassineboucham2002@gmail.com>
2024-10-14 14:43:33 +0100

    all

---
+++ 0x02-ES6_classes/9-main.js
@@ -0,0 +1,12 @@
+import listOfStudents, { StudentHolberton, HolbertonClass } from "./9-hoisting.js";
+
+console.log(listOfStudents);
+
+const listPrinted = listOfStudents.map(
+    student => student.fullStudentDescription
+);
+
+console.log(listPrinted)
+
+console.log(listOfStudents[0] instanceof StudentHolberton);
+console.log(listOfStudents[0].holbertonClass instanceof HolbertonClass);
