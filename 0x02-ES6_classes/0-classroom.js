0c970a19740f8d96cdd32b9635b6ea904face834
yassineboucham <yassineboucham2002@gmail.com>
2024-10-14 12:18:40 +0100

    0

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,10 @@
 export default class ClassRoom {
   constructor(maxStudentsSize) {
-    this._maxStudentsSizex = maxStudentsSize;
+    this._maxStudentsSize = maxStudentsSize;
   }
 }
+
+// console.log(ClassRoom.prototype.constructor === ClassRoom); // true
+// console.log(Object.getOwnPropertyNames(ClassRoom.prototype));
+// const room = new ClassRoom('R4');
+// console.log(room._maxStudentsSize);
