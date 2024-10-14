a2113f0b6d06628602b7d790d0d0e74e18e57af5
yassineboucham <yassineboucham2002@gmail.com>
2024-10-14 13:01:07 +0100

    7

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,1 +1,32 @@
-#!/usr/bin/node
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    this._location = value;
+  }
+
+  // When the class is cast into a String, it should return the location.
+  toString() {
+    return this._location;
+  }
+
+  // When the class is cast into a Number, it should return the size.
+  valueOf() {
+    return this._size;
+  }
+}
