7b2526f30a81a90b6e4ca1202847f265e6cd51c2
yassineboucham <yassineboucham2002@gmail.com>
2024-10-14 12:38:47 +0100

    5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,1 +1,37 @@
-#!/usr/bin/node
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    if (!(new.target === Building)) {
+      this.evacuationWarningMessage();
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() {
+    throw Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
+
+// const building = new Building(15);
+// console.log(building);
+// console.log(building.sqft);
+
+// class BuildingGood extends Building {
+//   // eslint-disable-next-line class-methods-use-this
+//   evacuationWarningMessage() {
+//     return 'Woof!';
+//   }
+// }
+
+// class BuildingBad extends Building { }
+
+// const testGood = new BuildingGood();
+// const testBad = new BuildingBad();4
