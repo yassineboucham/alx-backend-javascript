e0b4744d896b2c0026b611ff9c59ab6466f6a3b2
yassineboucham <yassineboucham2002@gmail.com>
2025-01-12 12:22:47 +0100

    1

--- 0x04-TypeScript/task_0/.eslintrc.js
+++ 0x04-TypeScript/task_0/.eslintrc.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+module.exports =  {
+  parser:  '@typescript-eslint/parser',
+  extends:  [
+    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
+  ],
+  parserOptions:  {
+    ecmaVersion:  2018,
+    sourceType:  'module',
+  },
+  rules:  {
+  },
+};
